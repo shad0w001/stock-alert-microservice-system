486cdcf130a9f39b4b26b7cf227aa74568c37e9d
1243557 <steli3117@gmail.com>
2026-03-13 17:22:27 +0200

    making the finnhub client a singleton

--- stock_monitor_service/src/main/java/com/internship/stock_monitor_service/clients/finnhub/FinnhubClient.java
+++ stock_monitor_service/src/main/java/com/internship/stock_monitor_service/clients/finnhub/FinnhubClient.java
@@ -4,14 +4,19 @@ import com.internship.stock_monitor_service.dtos.companies.FinnhubCompanyProfile
 import com.internship.stock_monitor_service.dtos.stocks.FinnhubStockQuoteResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class FinnhubClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     @Value("${finnhub.api.base-url}")
     private String baseUrl;
@@ -20,12 +25,30 @@ public class FinnhubClient {
     private String apiKey;
 
     public FinnhubCompanyProfileResponse getCompanyProfile(String symbol) {
-        String url = String.format("%s/stock/profile2?symbol=%s&token=%s", baseUrl, symbol, apiKey);
-        return restTemplate.getForObject(url, FinnhubCompanyProfileResponse.class);
+        String url = baseUrl + "/stock/profile2?symbol={symbol}&token={token}";
+        ResponseEntity<FinnhubCompanyProfileResponse> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<FinnhubCompanyProfileResponse>() {},
+                symbol,
+                apiKey
+        );
+        return response.getBody();
     }
 
     public FinnhubStockQuoteResponse getQuote(String symbol) {
-        String url = String.format("%s/quote?symbol=%s&token=%s", baseUrl, symbol, apiKey);
-        return restTemplate.getForObject(url, FinnhubStockQuoteResponse.class);
+        String url = baseUrl + "/quote?symbol={symbol}&token={token}";
+
+        ResponseEntity<FinnhubStockQuoteResponse> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<FinnhubStockQuoteResponse>() {},
+                symbol,
+                apiKey
+        );
+
+        return response.getBody();
     }
 }
