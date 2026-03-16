b2a1a88822a50a71e573eff47c6a597c0f9bd7c1
1243557 <steli3117@gmail.com>
2026-03-16 13:19:44 +0200

    enable shceduled jobs

--- stock_monitor_service/src/main/java/com/internship/stock_monitor_service/StockMonitorServiceApplication.java
+++ stock_monitor_service/src/main/java/com/internship/stock_monitor_service/StockMonitorServiceApplication.java
@@ -3,8 +3,10 @@ package com.internship.stock_monitor_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {
         "com.internship",
         "security",
