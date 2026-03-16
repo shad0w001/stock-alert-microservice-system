a2d91242a30e445b9928a0963b882a8273d69c38
1243557 <steli3117@gmail.com>
2026-03-16 10:51:09 +0200

    slight kafka topics refactor in alert service

---
+++ shared/src/main/java/topics/KafkaTopics.java
@@ -0,0 +1,6 @@
+package topics;
+
+public class KafkaTopics {
+    public static final String STOCK_ALERT_CREATIONS = "stock-alerts-creation-topic";
+    public static final String STOCK_ALERT_DELETIONS = "stock-alerts-deletion-topic";
+}
